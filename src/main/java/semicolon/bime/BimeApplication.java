1f296b3490d510cab676e0b6641e5d367297c1e7
james king <jamesasuelimen77@gmail.com>
2023-07-03 20:44:21 +0100

    initial commit

---
+++ src/main/java/semicolon/bime/BimeApplication.java
@@ -0,0 +1,13 @@
+package semicolon.bime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimeApplication.class, args);
+	}
+
+}
