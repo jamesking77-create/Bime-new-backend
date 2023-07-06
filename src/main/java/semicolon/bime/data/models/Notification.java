f87f91c3add357e11717e4dfa4b3ae703262d498
Felizdujadin <ezeikefelix51@gmail.com>
2023-07-06 08:10:29 +0100

    upload

--- src/main/java/semicolon/bime/data/models/Notification.java
+++ src/main/java/semicolon/bime/data/models/Notification.java
@@ -3,11 +3,13 @@ package semicolon.bime.data.models;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Document
 public class Notification {
     @Id
     private Long id;
--- src/main/java/semicolon/bime/data/models/User.java
+++ src/main/java/semicolon/bime/data/models/User.java
@@ -3,9 +3,11 @@ package semicolon.bime.data.models;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
+@Document
 public class User {
     @Id
     private Long id;
